77f9ef6bc6ed3d51bad3974669024d8610ba5ee9
duziaqin <duziaqin@gmail.com>
2017-03-06 23:31:18 +0800

    solved one ==

---
+++ 9_pack.hs
@@ -0,0 +1,17 @@
+-- Problem 9
+-- (**) Pack consecutive duplicates of list elements into sublists. If a list contains repeated elements they should be placed in separate sublists.
+-- 
+-- Example:
+-- 
+-- * (pack '(a a a a b c c a a d e e e e))
+-- ((A A A A) (B) (C C) (A A) (D) (E E E E))
+-- Example in Haskell:
+-- 
+-- *Main> pack ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 
+--              'a', 'd', 'e', 'e', 'e', 'e']
+--              ["aaaa","b","cc","aa","d","eeee"]')
+pack :: Eq a => [a] -> [[a]]
+pack = foldr func []
+    where func x [] = [[x]]
+          func x (y:ys) =
+              if x == (head y) then ((x:y):ys) else ([x]:y:ys)
