ffb919b86a195ac5ca1a905118961fe3257d4942
duziaqin <duziaqin@gmail.com>
2017-03-08 22:48:19 +0800

    solved three more

---
+++ 14_dupli.hs
@@ -0,0 +1,14 @@
+-- Problem 14
+-- (*) Duplicate the elements of a list.
+-- 
+-- Example:
+-- 
+-- * (dupli '(a b c c d))
+-- (A A B B C C C C D D)
+-- Example in Haskell:
+-- 
+-- > dupli [1, 2, 3]
+-- [1,1,2,2,3,3]')
+dupli :: [a] -> [a]
+dupli [] = []
+dupli (x:xs) = replicate 2 x ++ dupli xs
